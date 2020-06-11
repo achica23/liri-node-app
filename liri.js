99cbf9c5e2e21def321dbbd7081fcc14b4b12894
Ashley Chica <ahaven11@gmail.com>
2020-06-10 22:12:03 -0400

    update

---
+++ liri.js
@@ -0,0 +1,6 @@
+requestAnimationFrame("dotenv").config();
+var keys = require("/Users/ashleyhaven/Desktop/liri-node-app/keys.js");
+var spotify = new spotify(keys.spotify);
+var song = function (concert) {
+    this.concert = this.concert;
+}
