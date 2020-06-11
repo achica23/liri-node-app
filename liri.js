05e3826a30defb2dd787964dbffc866e6a20502b
Ashley Chica <ahaven11@gmail.com>
2020-06-10 23:39:38 -0400

    update

--- liri.js
+++ liri.js
@@ -1,6 +1,55 @@
-requestAnimationFrame("dotenv").config();
+require("dotenv").config();
 var keys = require("/Users/ashleyhaven/Desktop/liri-node-app/keys.js");
-var spotify = new spotify(keys.spotify);
-var song = function (concert) {
-    this.concert = this.concert;
+var Spotify = require("node-spotify-api");
+var spotify = new Spotify(keys.spotify);
+var axios = require("axios");
+var moment = require("moment");
+var command = process.argv[2]
+var userInput = process.argv.slice(3).join(' ')
+function liri() {
+    switch (command) {
+        case "concert-this":
+            concertThis()
+            break;
+        case "spotify-this-song":
+            spotifySong()
+            break;
+        case "movie-this":
+            movieThis()
+            break;
+        case "do-what-it-says":
+            doIt()
+            break;
+        default:
+            console.log('Please enter in one of these four commands')
+    }
+    function concertThis(search) {
+        console.log("Finding your concerts")
+
+    }
+    function spotifySong() {
+        if (!userInput) {
+            userInput = 'The Sign, Ace of Base';
+        }
+        var spotify = new Spotify(keys.spotify);
+        spotify.search({ type: 'track', query: userInput })
+
+            .then(function () { })
+
+    }
+    function movieThis() {
+        if (!userInput) {
+            userinput = 'Mr. Nobody'
+        }
+        var movieURL = 'http://www.omdbapi.com/?t=' + userInput + '&y=&plot=short&apikey=trilogy'
+        axios
+            .get(movieURL)
+            .then(function (response) {
+                let results = response.data
+                console.log(response)
+            })
+    }
+    function doIt() {
+
+    }
 }
